f1bd28fc6d95ad3ecc1113bef8c69d325d885b1f
Edson Sensato <esensato@hotmail.com>
2025-09-03 21:39:06 -0300

    03/09/2025

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { MongoClient, ObjectId } = require('mongodb');
-var express=require('express');
-var app=express();
+var express = require('express');
+var app = express();
 var cors = require('cors')
 var bodyParser = require('body-parser');
 
@@ -12,40 +12,42 @@ var corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 app.set('view engine', 'pug')
-app.use(bodyParser.json({limit: '500mb', extended: true}));
+app.use(bodyParser.json({ limit: '500mb', extended: true }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ extended: true, limit: '500mb' }));
 
 const uri = "mongodb+srv://teste:teste@cluster0.wnbdk2i.mongodb.net?retryWrites=true&w=majority";
 
 // --------- EXEMPLO ARMAZENAMENTO IMAGENS --------- 
 
 async function getImagens() {
-    const client = new MongoClient(uri);
-    try {
-      const imagem = client.db('imagem');
-      const fotos = imagem.collection('fotos');
-      var cursor = await fotos.find();
-      return await cursor.toArray();
-    } catch(e){
-      console.log(e);
-    } finally {
-      await client.close();
-    }
+  const client = new MongoClient(uri);
+  try {
+    const imagem = client.db('imagem');
+    const fotos = imagem.collection('fotos');
+    var cursor = await fotos.find();
+    return await cursor.toArray();
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
 }
 
 async function criarImagem(conteudo) {
   const client = new MongoClient(uri);
   try {
-      const imagemdb = client.db('imagem');
-      const fotos = imagemdb.collection('fotos');
-      await fotos.insertOne({imagem: conteudo});
+    const imagemdb = client.db('imagem');
+    const fotos = imagemdb.collection('fotos');
+    await fotos.insertOne({ imagem: conteudo });
   } catch (err) {
     console.log(err);
     return err;
   } finally {
-      await client.close();
-    }
-    return "OK";
+    await client.close();
   }
+  return "OK";
+}
 
 // --------- APP PEDIDO PIZZA --------- 
 
@@ -55,15 +57,15 @@ async function criarPizzas() {
   const client = new MongoClient(uri);
 
   try {
-      const pizzaria = client.db('pizzaria_app_pedido_pizza');
-      const cardapio = pizzaria.collection('pizzas');
-      await cardapio.insertOne({pizza: "Calabresa", preco: "R$ 25,00", imagem: 'url("https://www.sabornamesa.com.br/media/k2/items/cache/9189082f4804c1ab16e77d2cfe8d09d4_XL.jpg"'});
-      await cardapio.insertOne({pizza: "Quatro Queijos", preco: "R$ 45,00", imagem: 'url("https://pastapizza.com.br/wp-content/uploads/2017/07/Pizza-Pizzaria-Forno-Forza-Express.jpg")'});
-      await cardapio.insertOne({pizza: "Frango com Catupiry", preco: "R$ 50,00", imagem: 'url("https://swiftbr.vteximg.com.br/arquivos/ids/174176-768-768/pizza-artesanal-mussarela-swift-618284-1.jpg?v=637545446302470000")'});
-    } finally {
-      await client.close();
-    }
-    console.log('OK');
+    const pizzaria = client.db('pizzaria_app_pedido_pizza');
+    const cardapio = pizzaria.collection('pizzas');
+    await cardapio.insertOne({ pizza: "Calabresa", preco: "R$ 25,00", imagem: 'url("https://www.sabornamesa.com.br/media/k2/items/cache/9189082f4804c1ab16e77d2cfe8d09d4_XL.jpg"' });
+    await cardapio.insertOne({ pizza: "Quatro Queijos", preco: "R$ 45,00", imagem: 'url("https://pastapizza.com.br/wp-content/uploads/2017/07/Pizza-Pizzaria-Forno-Forza-Express.jpg")' });
+    await cardapio.insertOne({ pizza: "Frango com Catupiry", preco: "R$ 50,00", imagem: 'url("https://swiftbr.vteximg.com.br/arquivos/ids/174176-768-768/pizza-artesanal-mussarela-swift-618284-1.jpg?v=637545446302470000")' });
+  } finally {
+    await client.close();
+  }
+  console.log('OK');
 }
 
 async function criarPedido(pizza, quantidade, endereco) {
@@ -71,10 +73,10 @@ async function criarPedido(pizza, quantidade, endereco) {
   const client = new MongoClient(uri);
   let pedido;
 
-try {
+  try {
     const database = client.db('pizzaria_app_pedido_pizza');
     const pedidos = database.collection('pedidos');
-    pedido = await pedidos.insertOne({pizza: pizza, quantidade: quantidade, endereco: endereco});
+    pedido = await pedidos.insertOne({ pizza: pizza, quantidade: quantidade, endereco: endereco });
   } finally {
     await client.close();
   }
@@ -84,30 +86,30 @@ try {
 
 async function getPedidos() {
   const client = new MongoClient(uri);
-try {
-  const database = client.db('pizzaria_app_pedido_pizza');
-  const pedidos = database.collection('pedidos');
-  var cursor = pedidos.find();
-  return await cursor.toArray();
-} catch(e){
-  console.log(e);
-} finally {
-  await client.close();
-}
+  try {
+    const database = client.db('pizzaria_app_pedido_pizza');
+    const pedidos = database.collection('pedidos');
+    var cursor = pedidos.find();
+    return await cursor.toArray();
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
 }
 
 async function getPizzas() {
   const client = new MongoClient(uri);
-try {
-  const database = client.db('pizzaria_app_pedido_pizza');
-  const pizzas = database.collection('pizzas');
-  var cursor = pizzas.find();
-  return await cursor.toArray();
-} catch(e){
-  console.log(e);
-} finally {
-  await client.close();
-}
+  try {
+    const database = client.db('pizzaria_app_pedido_pizza');
+    const pizzas = database.collection('pizzas');
+    var cursor = pizzas.find();
+    return await cursor.toArray();
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
 }
 
 // --------- APP ADM PIZZARIA --------- 
@@ -117,18 +119,18 @@ async function criarPizza(idpizzaria, pizza, preco, imagem) {
   const client = new MongoClient(uri);
 
   try {
-      const pizzaria = client.db('pizzaria_app_adm_pizzaria');
-      const novaPizza = pizzaria.collection('pizza');
-      await novaPizza.insertOne({pizzaria: idpizzaria, pizza: pizza, preco: preco, imagem: imagem});
+    const pizzaria = client.db('pizzaria_app_adm_pizzaria');
+    const novaPizza = pizzaria.collection('pizza');
+    await novaPizza.insertOne({ pizzaria: idpizzaria, pizza: pizza, preco: preco, imagem: imagem });
   } catch (err) {
     console.log(err);
     return err;
-  }finally {
-      await client.close();
-    }
-    console.log('OK');
-    return "OK";
+  } finally {
+    await client.close();
   }
+  console.log('OK');
+  return "OK";
+}
 
 
 async function atualizarPizza(param_pizzaria, param_pizzaid, param_pizza, param_preco, param_imagem) {
@@ -136,49 +138,49 @@ async function atualizarPizza(param_pizzaria, param_pizzaid, param_pizza, param_
   const client = new MongoClient(uri);
 
   try {
-      const pizzaria = client.db('pizzaria_app_adm_pizzaria');
-      const novaPizza = pizzaria.collection('pizza');
-      await novaPizza.updateOne({_id: new ObjectId(param_pizzaid)}, { $set: {pizza: param_pizza, preco: param_preco, imagem: param_imagem}});
+    const pizzaria = client.db('pizzaria_app_adm_pizzaria');
+    const novaPizza = pizzaria.collection('pizza');
+    await novaPizza.updateOne({ _id: new ObjectId(param_pizzaid) }, { $set: { pizza: param_pizza, preco: param_preco, imagem: param_imagem } });
   } catch (err) {
     console.log(err);
     return err;
-  }finally {
-      await client.close();
-    }
-    console.log('OK');
-    return "OK";
+  } finally {
+    await client.close();
   }
+  console.log('OK');
+  return "OK";
+}
 
-  async function apagarPizza(idpizzaria, pizza) {
+async function apagarPizza(idpizzaria, pizza) {
 
-    const client = new MongoClient(uri);
-  
-    try {
-        const pizzaria = client.db('pizzaria_app_adm_pizzaria');
-        const novaPizza = pizzaria.collection('pizza');
-        await novaPizza.deleteOne({pizzaria: idpizzaria, pizza: pizza});
-    } catch (err) {
-      console.log(err);
-      return err;
-    }finally {
-        await client.close();
-      }
-      console.log('OK');
-      return "OK";
+  const client = new MongoClient(uri);
+
+  try {
+    const pizzaria = client.db('pizzaria_app_adm_pizzaria');
+    const novaPizza = pizzaria.collection('pizza');
+    await novaPizza.deleteOne({ pizzaria: idpizzaria, pizza: pizza });
+  } catch (err) {
+    console.log(err);
+    return err;
+  } finally {
+    await client.close();
+  }
+  console.log('OK');
+  return "OK";
 }
 
 async function getPizzasPizzaria(idpizzaria) {
   const client = new MongoClient(uri);
-try {
-  const pizzaria = client.db('pizzaria_app_adm_pizzaria');
-  const pizza = pizzaria.collection('pizza');
-  var cursor = pizza.find({pizzaria: idpizzaria});
-  return await cursor.toArray();
-} catch(e){
-  console.log(e);
-} finally {
-  await client.close();
-}
+  try {
+    const pizzaria = client.db('pizzaria_app_adm_pizzaria');
+    const pizza = pizzaria.collection('pizza');
+    var cursor = pizza.find({ pizzaria: idpizzaria });
+    return await cursor.toArray();
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
 }
 
 // --------- ENDPOINTS EXEMPLO ARMAZENAMENTO IMAGENS --------- 
@@ -215,17 +217,17 @@ app.get('/pizzas', async (req, res) => {
 
 // Cria um pedido de pizza
 app.post('/', async (req, res) => {
-    console.log(req.body);
-    const pedido = await criarPedido(req.body.pizza, req.body.quantidade, req.body.endereco);
-    res.send(pedido);
+  console.log(req.body);
+  const pedido = await criarPedido(req.body.pizza, req.body.quantidade, req.body.endereco);
+  res.send(pedido);
 })
 
 // Exibe uma página web com os pedidos realizados
 app.get('/', async (req, res) => {
   let ret = await getPedidos();
   res.render('index', {
-  pedidos: ret
-});(res);
+    pedidos: ret
+  }); (res);
 })
 
 // Retorna os pedidos no formato json
@@ -238,18 +240,18 @@ app.get('/json', async (req, res) => {
 // Apaga a pizza
 
 app.delete('/admin/pizza/:pizzariaid/:pizza', async (req, res) => {
-  
+
   const pizzaria = req.params.pizzariaid;
   const pizza = req.params.pizza;
 
   if (!pizzaria || pizzaria === "") {
     res.status(400);
-    res.send({erro: "Nome da pizzaria é obrigatório!"});
+    res.send({ erro: "Nome da pizzaria é obrigatório!" });
 
   }
   if (!pizza || pizza === "") {
     res.status(400);
-    res.send({erro: "Nome da pizza é obrigatório!"});
+    res.send({ erro: "Nome da pizza é obrigatório!" });
 
   }
 
@@ -276,22 +278,22 @@ app.post('/admin/pizza', async (req, res) => {
 
   if (!pizzaria || pizzaria === "") {
     res.status(400);
-    res.send({erro: "Nome da pizzaria é obrigatório!"});
+    res.send({ erro: "Nome da pizzaria é obrigatório!" });
 
   }
   if (!pizza || pizza === "") {
     res.status(400);
-    res.send({erro: "Nome da pizza é obrigatório!"});
+    res.send({ erro: "Nome da pizza é obrigatório!" });
 
   }
   if (!preco || preco === "") {
     res.status(400);
-    res.send({erro: "Preço da pizza é obrigatório!"});
+    res.send({ erro: "Preço da pizza é obrigatório!" });
 
   }
   if (!imagem || imagem === "") {
     res.status(400);
-    res.send({erro: "Imagem da pizza é obrigatório!"});
+    res.send({ erro: "Imagem da pizza é obrigatório!" });
 
   }
   const ret = await criarPizza(pizzaria, pizza, preco, imagem);
@@ -304,7 +306,7 @@ app.get("/admin/pizzas/:pizzariaid", async (req, res) => {
 
   if (!pizzaria || pizzaria === "") {
     res.status(400);
-    res.send({erro: "Nome da pizzaria é obrigatório!"});
+    res.send({ erro: "Nome da pizzaria é obrigatório!" });
 
   }
   res.send(await getPizzasPizzaria(pizzaria));
@@ -317,12 +319,12 @@ app.get('/ping', async (req, res) => {
 })
 
 app.listen(
-    { port: process.env.PORT | 3000, host: "0.0.0.0" },
-    function (err, address) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      console.log(`Your app is listening on ${process.env.PORT | 3000}`);
+  { port: process.env.PORT | 3000, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
     }
-  );
+    console.log(`Your app is listening on ${process.env.PORT | 3000}`);
+  }
+);
